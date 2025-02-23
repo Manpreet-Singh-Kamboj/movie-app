6bf74116428f5de8ce37414f9dad0f9ee346041d
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-02-23 20:44:32 +0530

    Refactoring

--- src/_components/common/Search.jsx
+++ src/_components/common/Search.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const Search = () => {
-  return <div className="text-white">Search</div>;
+  const [searchValue, setSearchValue] = useState("");
+  return (
+    <>
+      <div className="w-40 sm:w-60">
+        <input
+          type="text"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Search movies"
+          className="bg-gray-300 px-3  sm:px-5 py-1 rounded-full w-full sm:w-60"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Search;
